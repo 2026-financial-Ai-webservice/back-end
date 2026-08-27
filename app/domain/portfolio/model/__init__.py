6a346070cb36bd7b3fcc45983b8275cea5ade7ed
corjqnrl <coqnrl@ewha.ac.kr>
2026-08-27 21:19:37 +0900

    REFACTOR(#17): portfolio_results, portfolio_result_companies 경로 수정

--- app/domain/portfolio/model/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.domain.portfolio.model.portfolioResultModel import PortfolioResult
-from app.domain.portfolio.model.portfolioResultCompany import PortfolioResultCompany
-
-__all__ = ["PortfolioResult", "PortfolioResultCompany"]
---
+++ app/domain/portfolio/models/__init__.py
@@ -0,0 +1,4 @@
+from app.domain.portfolio.models.portfolioResultModel import PortfolioResult
+from app.domain.portfolio.models.portfolioResultCompany import PortfolioResultCompany
+
+__all__ = ["PortfolioResult", "PortfolioResultCompany"]
