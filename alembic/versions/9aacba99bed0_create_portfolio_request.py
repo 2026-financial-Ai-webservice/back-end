85a994181b33e49c2cd74c489f892c42af9604cb
jade <syeong8663@gmail.com>
2026-08-28 22:31:53 +0900

    fix: lint ruff

--- alembic/versions/9aacba99bed0_create_portfolio_request.py
+++ alembic/versions/9aacba99bed0_create_portfolio_request.py
@@ -5,17 +5,17 @@ Revises: ef4a6e6a76b8
 Create Date: 2026-08-28 22:27:30.533999
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '9aacba99bed0'
-down_revision: Union[str, Sequence[str], None] = 'ef4a6e6a76b8'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = 'ef4a6e6a76b8'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 
